884b3a95536ce9d0ee0ba8bae06e8b49aa1f6968
sankar patra <sankarpatra573@gmail.com>
2022-11-04 09:59:21 +0530

    done

---
+++ App.js
@@ -0,0 +1,60 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/mydb',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res)=>{
+    var name = req.body.name;
+    var phone = req.body.phone;
+    var age = req.body.age;
+    var pin = req.body.pin;
+    var adhar=req.body.adhar;
+    var pasword=req.body.pasword;
+
+    var data = {
+        "name": name,
+        "phone" : phone,
+        "age": age,
+        "adhar" : adhar,
+        "pin":pin,
+        "pasword":pasword
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('signup_success.html')
+
+})
+
+
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('index.html');
+}).listen(3000);
+
+
+console.log("Listening on PORT 3000");
